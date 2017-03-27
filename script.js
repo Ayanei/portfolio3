56591c47a021fe140bf3e72107e0575fd66f7d0a
Ayanei <orane.mas@gmail.com>
2017-03-28 00:43:25 +0200

    WIP

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $( "#hide-card" ).click(function() {
-  		$( ".hide-card" ).css("border", "2px solid orange");
+    $( "#bio" ).hide();
+
+    $( "#bio-link" ).click(function() {
+  		$( "#portfolio" ).hide();
+  		$( "#bio" ).fadeIn("slow");
 	});
+
+	$( "#portfolio-link" ).click(function() {
+  		$( "#bio" ).hide();
+  		$( "#portfolio" ).fadeIn("slow");
+
+	});
+
+	$( ".uk-card" ).hover(
+	  function() {
+	    $( this ).append( $( "<div class='uk-overlay-default uk-position-cover'></div>" ) );
+	  }, function() {
+	    $( this ).find( ".uk-overlay-default:last" ).remove();
+	  }
+	);
+
+
 });
